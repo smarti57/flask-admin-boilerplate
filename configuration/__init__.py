39ce9faa3ed006a409d26e9d4518d7733ee49659
Scott Martin <scott.martin@janusresearch.com>
2022-04-08 15:14:28 -0400

    Try 2

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -9,14 +9,14 @@ app.secret_key = "ITSASECRET"
 app.config['MAIL_SERVER']='192.168.1.1' #mail server
 app.config['MAIL_PORT'] = 587 #mail port
 app.config['MAIL_USERNAME'] = 'smartin' #email
-app.config['MAIL_PASSWORD'] = process.env['MAIL_PASSWORD'] #password
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') #password
 app.config['MAIL_USE_TLS'] = True #security type
 app.config['MAIL_USE_SSL'] = False #security type
 
 #database connection parameters
 connection_params = {
     'user': 'flaskdb_user',
-    'password': process.env['DB_PASSWORD'],
+    'password': os.environ.get('DB_PASSWORD'),
     'host': 'cluster0.xrgdk.mongodb.net',
     'port': '27017',
     'namespace': 'myFirstDatabase',
