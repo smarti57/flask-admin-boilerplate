b4b7481af931ace547b00cf188a94cc47f8f820d
Scott Martin <scott.martin@janusresearch.com>
2022-04-11 13:53:18 -0400

    Added port to URL

--- helpers/database.py
+++ helpers/database.py
@@ -5,7 +5,7 @@ connection_params = configuration.connection_params
 
 #connect to mongodb
 mongoconnection = MongoClient(
-    'mongodb+srv://{user}:{password}@{host}'
+    'mongodb+srv://{user}:{password}@{host}:{port}'
     '/{namespace}?retryWrites=true&w=majority'.format(**connection_params)
 )
 
@@ -13,5 +13,3 @@ mongoconnection = MongoClient(
     #'mongodb+srv://{user}:{password}@{host}:'
     #'{port}/{namespace}?retryWrites=false'.format(**connection_params)
 #pymongo.MongoClient("mongodb+srv://flaskdb_user:<password>@cluster0.xrgdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-db = mongoconnection.dashboardDb
