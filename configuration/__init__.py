0295fb7c3dd3764b8bd4ecf08d4112ca2974201b
Joseph Martin <joseph.s.martin70.ctr@mail.mil>
2022-04-12 14:44:48 -0400

    updated config

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -9,17 +9,22 @@ app.secret_key = "ITSASECRET"
 app.config['MAIL_SERVER']='192.168.1.1' #mail server
 app.config['MAIL_PORT'] = 587 #mail port
 app.config['MAIL_USERNAME'] = 'smartin' #email
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') #password
+#app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') #password
+app.config['MAIL_PASSWORD'] = 'mhonet' #password
 app.config['MAIL_USE_TLS'] = True #security type
 app.config['MAIL_USE_SSL'] = False #security type
 
 #database connection parameters
 connection_params = {
     'user': 'flaskdb_user',
-    'password': os.environ.get('DB_PASSWORD'),
+   # 'password': os.environ.get('DB_PASSWORD'),
+    'password': 'HzZnJUgDiDkQ6dc3',
     'host': 'cluster0.xrgdk.mongodb.net',
     'port': '27017',
     'namespace': 'myFirstDatabase',
 }
 #PyBytesToken
 #eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1Ijoic2NvdHQubWFydGluQGphbnVzcmVzZWFyY2guY29tIiwidCI6IjZiZGI3MGJlNmQ3YzQyZmI4MDg0YTYyMGJiMDFhZjIyIn0.IB85Stw0heASAuXqAaW6AdjNA4bNgr3kbWQer57xNLk
+
+
+#HzZnJUgDiDkQ6dc3
